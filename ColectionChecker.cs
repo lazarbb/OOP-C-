3db777895e6c1bc2535336046c9db7e95c6acb09
lazarbb <lazarv79@gmail.com>
2023-05-10 22:42:18 +0200

    OOP-C#

---
+++ ColectionChecker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    class ColectionChecker
+    {
+        public static bool HasDuplicate(YouTubeVideo[] videos)
+        {
+            int i, j, br=0;
+
+            for (i = 0; i < videos.Length; i++)
+            {
+                for (j = i + 1; j < videos.Length; j++)
+                {
+                    if (videos[i] == videos[j]) { br++; }
+
+                }
+            }
+
+            if (br > 0) return true;
+            return false;
+        }
+    }
+}
---
+++ SolarPanel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Test
+{
+    class SolarPanel
+    {
+        protected double width;
+        protected double height;
+        protected double efficiency;
+
+        public SolarPanel(double width, double height, double efficiency)
+        {
+            this.width = width;
+            this.height = height;
+            this.efficiency = efficiency;
+        }
+
+        public SolarPanel()
+        {
+            this.width = 1;
+            this.height = 1;
+            this.efficiency = 0.5;
+        }
+
+        public override string ToString()
+        {
+            return $"{width}, {height}, {efficiency}";
+        }
+
+        public virtual double CalculateAnnualEfficiency()
+        {
+            return width * height * efficiency * 1300;
+        }
+    }
+}
